6a3b120755dc0a4484ffdcb82677ffa7ca1ec012
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-05-20 13:10:45 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :hooks [leiningen.cljsbuild]
   
   :cljsbuild
