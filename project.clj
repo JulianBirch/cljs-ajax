1807e4b83c337d3c28a356cdea3ec7fb521cd014
Julian Birch <julian.birch@gmail.com>
2016-05-23 21:36:16 +0100

    Updated to 0.5.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-ajax "0.5.4"
+(defproject cljs-ajax "0.5.5"
   :min-lein-version "2.5.2" ;;; lower can't run tests in cljc
   :description "A simple Ajax library for ClojureScript"
   :url "https://github.com/JulianBirch/cljs-ajax"
