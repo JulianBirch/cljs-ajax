74baec0194938ec6290145df394a31728bb9a391
Julian Birch <julian.birch@gmail.com>
2026-04-03 10:03:37 +0100

    build: add deps.edn and JVM test runner

---
+++ test/ajax/test/jvm_runner.clj
@@ -0,0 +1,16 @@
+(ns ajax.test.jvm-runner
+  (:require [clojure.test :as t]))
+
+(def test-namespaces
+  ['ajax.test.core
+   'ajax.test.server-interaction
+   'ajax.test.url])
+
+(defn run-tests [_]
+  (doseq [test-namespace test-namespaces]
+    (require test-namespace))
+  (let [{:keys [fail error]} (apply t/run-tests test-namespaces)]
+    (when (pos? (+ fail error))
+      (throw (ex-info "JVM tests failed"
+                      {:fail fail
+                       :error error})))))
