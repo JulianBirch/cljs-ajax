1323a56fc96a9be50af76297d9811da5f2a62053
Julian Birch <julian.birch@gmail.com>
2026-04-03 10:14:34 +0100

    build: migrate CLJS builds to shadow-cljs

--- test/ajax/test/runner.cljs
+++
@@ -1,7 +0,0 @@
-(ns ajax.test.runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [ajax.test.core]
-            [ajax.test.url]))
-
-(doo-tests 'ajax.test.core
-           'ajax.test.url)
