fcd7074ab22f0f9dd3711db04167013c1591033f
Pedro Gomes Branquinho <pedrogbranquinho@gmail.com>
2026-08-11 14:58:19 -0300

    Migrate the JVM implementation to Apache HttpClient 5
    HttpAsyncClient 4 is end of life. This moves the implementation to the
    HttpClient 5 asynchronous API.
    
    Requests use AsyncRequestBuilder, which takes the method as a string, so
    arbitrary HTTP methods still work without a class per method. Bodies map
    onto entity producers: byte arrays and strings pass through, files use
    FileEntityProducer, input streams use the classic producer rather than
    being buffered. Responses use SimpleResponseConsumer, which buffers the
    body like the 4.x consumer, and HttpResponseWrapper adapts a
    SimpleHttpResponse.
    
    Connect and socket timeouts belong to the connection manager's
    ConnectionConfig in 5.x rather than to RequestConfig. An absent or zero
    timeout maps to Timeout/DISABLED, since 5.x defaults the connect timeout
    to three minutes where 4.x left it unbounded.
    
    A connect timeout now reports status -1, like a socket timeout.
    
    HttpClient 5 ships only the RFC 6265 cookie specifications, so :default,
    :netscape and :standard select the relaxed policy and :standard-strict
    the strict one.

--- src/ajax/apache.clj
+++ src/ajax/apache.clj
@@ -1,20 +1,32 @@
 (ns ajax.apache
   (:require [ajax.protocols :refer [map->Response
               AjaxImpl AjaxRequest AjaxResponse]]
+            [clojure.java.io :as io]
             [clojure.string :as s])
   (:import [clojure.lang IDeref IBlockingDeref IPending]
-           [org.apache.http HttpResponse Header]
-           [org.apache.http.entity ByteArrayEntity StringEntity
-            FileEntity InputStreamEntity]
-           [org.apache.http.client.methods HttpRequestBase
-            HttpEntityEnclosingRequestBase]
-           [org.apache.http.client.config RequestConfig CookieSpecs]
-           [org.apache.http.concurrent FutureCallback]
-           [org.apache.http.impl.nio.client HttpAsyncClients]
+           [org.apache.hc.client5.http ConnectTimeoutException]
+           [org.apache.hc.client5.http.async.methods SimpleHttpResponse
+            SimpleResponseConsumer]
+           [org.apache.hc.client5.http.config ConnectionConfig RequestConfig]
+           [org.apache.hc.client5.http.cookie StandardCookieSpec]
+           [org.apache.hc.client5.http.impl.async CloseableHttpAsyncClient
+            HttpAsyncClients]
+           [org.apache.hc.client5.http.impl.nio
+            PoolingAsyncClientConnectionManagerBuilder]
+           [org.apache.hc.core5.concurrent FutureCallback]
+           [org.apache.hc.core5.http ContentType Header]
+           [org.apache.hc.core5.http.nio AsyncEntityProducer
+            AsyncRequestProducer]
+           [org.apache.hc.core5.http.nio.entity AsyncEntityProducers]
+           [org.apache.hc.core5.http.nio.support AsyncRequestBuilder]
+           [org.apache.hc.core5.http.nio.support.classic
+            AbstractClassicEntityProducer]
+           [org.apache.hc.core5.util Timeout]
            [java.lang Exception]
            [java.util.concurrent Future]
            [java.net URI SocketTimeoutException]
-           [java.io File InputStream Closeable]))
+           [java.io ByteArrayInputStream File InputStream OutputStream
+            Closeable]))
 
 ;;; Chunks of this code liberally ripped off dakrone/clj-http
 ;;; Although that uses the synchronous API
@@ -23,16 +35,28 @@
 
 (def array-of-bytes-type (Class/forName "[B"))
 
-(defn- to-entity 
+(def ^:private ^ContentType text-content-type
+  (ContentType/create "text/plain" "UTF-8"))
+
+(defn- stream-entity-producer
+  "HttpClient 5 has no entity producer for an InputStream, so we use
+   the classic (blocking) producer, which pumps the stream on a
+   separate thread rather than buffering it in memory."
+  ^AsyncEntityProducer [^InputStream in]
+  (proxy [AbstractClassicEntityProducer] [8192 nil clojure.lang.Agent/soloExecutor]
+    (produceData [_content-type ^OutputStream out]
+      (io/copy in out))))
+
+(defn- to-entity-producer
   "This function means you can just hand cljs-ajax a byte
    array, string, normal Java file or input stream and it
    will automatically work with the Apache implementation."
-  [b]
+  ^AsyncEntityProducer [b]
   (condp instance? b
-    array-of-bytes-type (ByteArrayEntity. b)
-    String (StringEntity. ^String b "UTF-8")
-    File (FileEntity. b)
-    InputStream (InputStreamEntity. b)
+    array-of-bytes-type (AsyncEntityProducers/create ^bytes b nil)
+    String (AsyncEntityProducers/create ^String b text-content-type)
+    File (AsyncEntityProducers/create ^File b nil)
+    InputStream (stream-entity-producer b)
     b))
 
 (defn- to-uri [u]
@@ -49,40 +73,46 @@
 ;;; So, you end up doing what you'd normally do in Java,
 ;;; write an adapter class.
 
-;;; Takes an HttpResponse and exposes the interface needed
+;;; Takes a SimpleHttpResponse and exposes the interface needed
 ;;; by cljs-ajax interceptors (including response formats).
 
-(defrecord HttpResponseWrapper [^HttpResponse response]
+(defrecord HttpResponseWrapper [^SimpleHttpResponse response]
   AjaxResponse
   (-body [this]
-    (let [^HttpResponse response (:response this)]
-      (some-> response .getEntity .getContent)))
+    (let [^SimpleHttpResponse response (:response this)]
+      (some-> (.getBodyBytes response) (ByteArrayInputStream.))))
   (-status [this]
-    (let [^HttpResponse response (:response this)]
-      (-> response .getStatusLine .getStatusCode)))
+    (let [^SimpleHttpResponse response (:response this)]
+      (.getCode response)))
   (-status-text [this]
-    (let [^HttpResponse response (:response this)]
-      (-> response .getStatusLine .getReasonPhrase)))
+    (let [^SimpleHttpResponse response (:response this)]
+      (.getReasonPhrase response)))
   (-get-all-headers [this]
-    (let [^HttpResponse response (:response this)]
+    (let [^SimpleHttpResponse response (:response this)]
       (reduce (fn [headers ^Header header]
                 (assoc headers (.getName header) (.getValue header)))
               {}
-              (.getAllHeaders response))))
+              (.getHeaders response))))
   (-get-response-header [this header]
-    (let [^HttpResponse response (:response this)]
-      (.getValue (.getFirstHeader response header))))
+    (let [^SimpleHttpResponse response (:response this)]
+      (.getValue (.getFirstHeader response ^String header))))
   (-was-aborted [this] false))
 
 (defn- create-request
-  "Life's to short to use all of the apache types for
-   the different HTTP methods when you can just wrap a
-   string in the appropriate base class."
-  ^HttpEntityEnclosingRequestBase [method]
-  (proxy [HttpEntityEnclosingRequestBase] []
-    (getMethod [] method)))
+  "Builds the request producer the Apache async API executes.
+   `AsyncRequestBuilder` takes the method as a string, so all
+   HTTP methods are supported without a class per method."
+  ^AsyncRequestProducer [{:keys [uri method body headers]}]
+  (let [builder (AsyncRequestBuilder/create method)]
+    (.setUri builder ^URI (to-uri uri))
+    (when-let [entity (to-entity-producer body)]
+      (.setEntity builder ^AsyncEntityProducer entity))
+    (doseq [x headers]
+      (let [[h v] x]
+        (.addHeader builder ^String h ^String v)))
+    (.build builder)))
 
-(defn- cancel 
+(defn- cancel
   "This method ensures that the behaviour of the wrapped
    Apache classes matches the behaviour the javascript version,
    including the negative status number."
@@ -93,10 +123,14 @@
                    :headers {}
                    :was-aborted true})))
 
+(defn- timeout? [ex]
+  (or (instance? SocketTimeoutException ex)
+      (instance? ConnectTimeoutException ex)))
+
 (defn- fail [handler ^Exception ex]
   "XMLHttpRequest reports a status of -1 for timeouts, so
    we do the same."
-  (let [status (if (instance? SocketTimeoutException ex) -1 0)]
+  (let [status (if (timeout? ex) -1 0)]
     (handler
      (map->Response {:status status
                      :status-text (.getMessage ex)
@@ -104,7 +138,7 @@
                      :exception ex
                      :was-aborted false}))))
 
-(defn- create-handler 
+(defn- create-handler
    "Takes a cljs-ajax style handler method and converts it
    to a FutureCallback suitable for use the Apache API."
   [handler]
@@ -124,26 +158,49 @@
   (fn get-cookie-dispatch [request] (:cookie-policy request)))
 
 (defmethod get-cookie-policy :none none-cookie-policy
-  [_] CookieSpecs/IGNORE_COOKIES)
+  [_] StandardCookieSpec/IGNORE)
 (defmethod get-cookie-policy :default default-cookie-policy
-  [_] CookieSpecs/DEFAULT)
+  [_] StandardCookieSpec/RELAXED)
 (defmethod get-cookie-policy :netscape netscape-cookie-policy
-  [_] CookieSpecs/NETSCAPE)
+  [_] StandardCookieSpec/RELAXED)
 (defmethod get-cookie-policy :standard standard-cookie-policy
-  [_] CookieSpecs/STANDARD)
+  [_] StandardCookieSpec/RELAXED)
 (defmethod get-cookie-policy :standard-strict standard-strict-cookie-policy
-  [_] CookieSpecs/STANDARD_STRICT)
+  [_] StandardCookieSpec/STRICT)
+
+(defn- to-timeout
+  "Zero or absent means no timeout, matching XMLHttpRequest."
+  ^Timeout [ms]
+  (if (and ms (pos? ms))
+    (Timeout/ofMilliseconds (long ms))
+    Timeout/DISABLED))
+
+(defn- create-connection-config
+  ^ConnectionConfig [{:keys [timeout socket-timeout]}]
+  (-> (ConnectionConfig/custom)
+      (.setConnectTimeout (to-timeout timeout))
+      (.setSocketTimeout (to-timeout (or socket-timeout timeout)))
+      (.build)))
 
 (defn- create-request-config
-  ^RequestConfig [{:keys [timeout socket-timeout cookie-policy] :as req}]
+  ^RequestConfig [{:keys [cookie-policy] :as req}]
   (let [builder (RequestConfig/custom)]
-    (if timeout
-      (.setConnectTimeout builder timeout))
-    (if-let [st (or socket-timeout timeout)]
-      (.setSocketTimeout builder st))
     (if cookie-policy
-      (.setCookieSpec builder (get-cookie-policy req)))
+      (.setCookieSpec builder ^String (get-cookie-policy req)))
     (.build builder)))
+
+(defn- create-client
+  "The connection manager owns the connect and socket timeouts in
+   HttpClient 5; it is not shared, so closing the client closes it."
+  ^CloseableHttpAsyncClient [opts]
+  (let [connection-manager (-> (PoolingAsyncClientConnectionManagerBuilder/create)
+                               (.setDefaultConnectionConfig
+                                (create-connection-config opts))
+                               (.build))]
+    (-> (HttpAsyncClients/custom)
+        (.setConnectionManager connection-manager)
+        (.setDefaultRequestConfig (create-request-config opts))
+        (.build))))
 
 (defn- to-clojure-future
   "Converts a normal Java future to one similar to the one generated
@@ -188,21 +245,15 @@
 (defrecord Connection []
   AjaxImpl
   (-js-ajax-request
-    [this {:keys [uri method body headers] :as opts} handler]
+    [this opts handler]
     (try
-      (let [request (doto (create-request method)
-                      (.setURI (to-uri uri))
-                      (.setEntity (to-entity body)))
-            request-config (create-request-config opts)
-            builder (doto (HttpAsyncClients/custom)
-                      (.setDefaultRequestConfig request-config))
-            client (doto (.build builder)
-                     (.start))
-            h (create-handler handler)]
-        (doseq [x headers]
-          (let [[h v] x]
-            (.addHeader request h v)))
-        (to-clojure-future (.execute client request h) client))
+      (let [request (create-request opts)
+            ^CloseableHttpAsyncClient client (create-client opts)
+            ^FutureCallback h (create-handler handler)]
+        (.start client)
+        (to-clojure-future
+         (.execute client request (SimpleResponseConsumer/create) h)
+         client))
       (catch Exception ex (fail handler ex)))))
 
 (defn new-api
